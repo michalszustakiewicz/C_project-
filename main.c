cc737008da3c9994d952a19cabada5f3540c335d
michalszustakiewicz <40272497+michalszustakiewicz@users.noreply.github.com>
2018-11-16 20:34:13 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int wiek=24;
+    printf("Hello World! \n");
+    printf("Mam %d lata\n Za rok bedzie %d lat",wiek,wiek+1); // %d odpowiada za podstawienie wartoœci po przecinku
+                                                                // odpowiedniej do kolejnoœci 1 d - wiek 2 d - wiek +1
+    return 0;
+
+}
+
